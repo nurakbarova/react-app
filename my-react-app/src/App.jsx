e520910fe2c4da5b8fa5430f22bca772cbdfcace
nurakbarova <nurayakbarova76@gmail.com>
2023-05-05 04:05:27 +0400

    react

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css';
+import { Autocomplete, Stack, TextField } from "@mui/material";
+
+
+function App() {
+  return (
+    <>
+         <Stack spacing={2} sx={{ width: 300 }}>
+      <Autocomplete
+        id="free-solo-demo"
+        freeSolo
+        options={top100Films.map((option) => option.title)}
+        renderInput={(params) => <TextField {...params} label="freeSolo" />}
+      />
+      <Autocomplete
+        freeSolo
+        id="free-solo-2-demo"
+        disableClearable
+        options={top100Films.map((option) => option.title)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search input"
+            InputProps={{
+              ...params.InputProps,
+              type: 'search',
+            }}
+          />
+        )}
+      />
+    </Stack>
+
+     
+
+    </>
+
+  );
+}
+
+export default App;
